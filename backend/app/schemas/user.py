029de8e1f8d66ec12722eee0a8a577f4f1e199d2
nNm205 <minh2m5@gmail.com>
2026-05-13 11:28:07 +0700

    feat(auth): implement authentication schemas and password security

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,40 @@
+from pydantic import ( 
+    BaseModel, 
+    EmailStr, 
+    Field,
+    model_validator
+) 
+
+class UserRegister(BaseModel):
+    email: EmailStr
+
+    username: str = Field(
+        min_length=3,
+        max_length=30 
+    )
+
+    password: str = Field(
+        min_length=6
+    )
+
+    confirm_password: str 
+
+    @model_validator(mode="after")
+    def validate_passwords_match(self):
+        if self.password != self.confirm_password:
+            raise ValueError("Passwords do not match")
+        
+        return self 
+
+class UserLogin(BaseModel): 
+    email: EmailStr
+    password: str 
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr 
+    username: str 
+
+class TokenResponse(BaseModel):
+    access_token = str 
+    user: UserResponse 
