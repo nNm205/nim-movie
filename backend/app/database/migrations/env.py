839296f827f2130f83c635b460cac554fbe792bf
nNm205 <minh2m5@gmail.com>
2026-05-13 11:35:11 +0700

    chore: configure Alembic environment variables

--- backend/app/database/migrations/env.py
+++ backend/app/database/migrations/env.py
@@ -6,11 +6,16 @@ from sqlalchemy import pool
 from alembic import context
 
 from app.database.base import Base 
+from app.config import settings
 import app.models 
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.DATABASE_URL
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
