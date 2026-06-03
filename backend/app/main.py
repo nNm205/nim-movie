cab430a264cab71c142d7a2a3b394663d78f2d46
nNm205 <minh2m5@gmail.com>
2026-06-03 21:43:12 +0700

    fix: fix CORS origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://nim-movie.vercel.app/"],
+    allow_origins=["https://nim-movie.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
